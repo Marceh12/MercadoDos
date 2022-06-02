95b13e3908334328b42129c3db91156534f97734
Marceh12 <103226063+Marceh12@users.noreply.github.com>
2022-06-02 20:58:11 -0300

    procfile creado

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const path = require ('path');
 const publicPath = path.resolve (__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen (3030, ()=> console.log ('servidor corriendo en puerto 3030'));
+// app.listen (3030, ()=> console.log ('servidor corriendo en puerto 3030'));
+
+app.listen(process.env.PORT|| 3000, function(){
+    console.log("servidor corriendo en el puerto 3000")
+})
+
 
 app.get ('/', (req,res)=>{
     res.sendFile(path.join (__dirname, '/views/home.html'));}
